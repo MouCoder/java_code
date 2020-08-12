75738ed21694ef9b3f722a6ff05df04f141960fc
MouCoder <2241059521@qq.com>
2020-08-12 13:44:34 +0800

    最长无重复子串长度（提交成功代码）

---
+++ lengthOfLongestSubstringMethod1/Test.java
@@ -0,0 +1,8 @@
+package LeetCode.String.lengthOfLongestSubstringMethod1;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.lengthOfLongestSubstringMethod1("abcabcbb"));
+    }
+}
